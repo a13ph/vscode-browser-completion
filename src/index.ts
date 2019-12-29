98158292ef1ee80f8b236b2c2c806abeae8818d8
voldikss <dyzplus@gmail.com>
2019-12-29 23:25:20 +0800

    enable for all filetypes by default

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
   let { subscriptions } = context
 
   const config = vscode.workspace.getConfiguration('browserCompletion')
-  const filetypes = config.get<string[]>('filetypes', ['plaintext'])
+  const filetypes = config.get<string[]>('filetypes', ['*'])
   let capacity = config.get<number>('capacity', 5)
   if (capacity <= 0 || capacity >= 10) {
     capacity = 5
